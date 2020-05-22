a7f013da8558b31eb871cb0c7a10ddb487fb8ba6
Laura Gutierrez Funderburk <lfunderburk@users.noreply.github.com>
2020-05-22 13:24:36 -0700

    add TSV to JSON python converter

---
+++ scripts/tsv_to_json.py
@@ -0,0 +1,23 @@
+import csv
+import os
+import json
+import pandas as pd
+import sys
+
+args    = sys.argv[1:]
+tsvpath = args[0]
+try:
+    dataframe = pd.read_csv(tsvpath, sep='\t')
+except:
+    print("\n\nFile {} not found.".format(tsvpath),
+          "\nUsage: python3 tsv_to_json.py [path to tsv file]"
+          "\nExiting.\n\n\n")
+
+    raise FileNotFoundError
+    exit
+
+out      = dataframe.to_json(orient='records')
+jsonpath = tsvpath[:-3]+"json"
+with open(jsonpath, 'w') as f: 
+    f.write(out)
+    print("Written successfully to {}".format(jsonpath))
